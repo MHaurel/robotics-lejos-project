61d292c121f5c954bd818a397e1723d7cd9163c4
MHaurel <maxime.haurel0@gmail.com>
2022-10-07 08:30:06 +0200

    Map array

--- src/project/Map.java
+++ src/project/Map.java
@@ -5,12 +5,22 @@ import java.io.Serializable;
 import project.Test.Color;
 
 public class Map implements Serializable {
-
-	Color[][] map = new Color[7][5];
 	
-	public Map() {
-		
+	enum Color {
+		RED, GREEN, BLUE, ORANGE, WHITE, BLACK
 	}
+	
+	Color[][] map = {
+			{Color.BLUE, Color.BLUE,   Color.BLUE,   Color.BLUE,   Color.BLUE},
+			{Color.BLUE, Color.ORANGE, Color.GREEN,  Color.GREEN,  Color.RED},
+			{Color.BLUE, Color.ORANGE, Color.BLUE,   Color.GREEN,  Color.BLUE},
+			{Color.BLUE, Color.GREEN,  Color.WHITE,  Color.GREEN,  Color.BLUE},
+			{Color.BLUE, Color.GREEN,  Color.ORANGE, Color.ORANGE, Color.BLUE},
+			{Color.BLUE, Color.GREEN,  Color.GREEN,  Color.GREEN,  Color.BLUE},
+			{Color.RED,  Color.GREEN,  Color.BLUE,   Color.BLUE,   Color.BLUE},
+	}; 
+	
+	public Map() {}
 	
 	public Color[][] getMap() {
 		return this.map;
