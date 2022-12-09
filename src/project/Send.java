7bfea0515a7898ff2e7ef3549e4bd456b25000ad
Max <54206618+MHaurel@users.noreply.github.com>
2022-12-09 18:21:23 +0100

    Small edit to code

--- src/project/Send.java
+++ src/project/Send.java
@@ -22,7 +22,7 @@ public class Send implements Behavior {
 	@Override
 	public boolean takeControl() {
 		// TODO Auto-generated method stub
-		System.out.println("I'm the receiver !");
+		System.out.println("I'm the sender !");
 		return this.pilot.getStatus() == Status.SENDER;
 	}
 
